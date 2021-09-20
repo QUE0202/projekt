fa17bc65b308fca97c868fec95f6f1dafd579f7b
que2115 <jakub1016@gmail.com>
2021-09-20 15:16:50 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    //res.send("Strona działa");
+    res.render('index', {});
+});
+
+app.get('/omnie', (req, res) => {
+    res.send("Strona działa");
+});
+
+app.get('/kontakt', (req, res) => {
+    res.send("Strona działa");
+});
+
+app.listen(5000, () => {
+    console.log("Serwer działa");
+});
